42117a5a35507681c27d6dbae7d9817cbd2fdfbf
Marcy Calderon <marcycalderon@hotmail.co.uk>
2022-03-02 16:39:58 +0000

    complete objects Qs 1-5

--- objects.js
+++ objects.js
@@ -1,3 +1,4 @@
+// node objects.js
 /**
  * In these tasks you will need to manipulate the object below in order to answer the questions. Some answers will require you to store the result in a variable, others will need you to inspect the whole array.
  */
@@ -30,3 +31,43 @@ tortoise = {
  dogName = dog.name;
 
  console.log("The dog's name is " + dogName);
+
+
+ /**
+ * Q2. What breed is the cat?
+ */
+
+catBreed = cat.breed;
+
+console.log("The cat is a " + catBreed);
+
+
+/**
+ * Q3. It's the tortoise's birthday! Add one to her current age.
+ */
+
+tortoise.age = tortoise.age + 1;
+
+console.log(tortoise)
+
+/**
+ * Q4. How many different colours is Poppy?
+ */
+
+dogColourCount = dog.colours.length;
+
+console.log("The dog has " + dogColourCount + " different colours");
+
+
+/**
+ * Q5. I've just bought a 12-year-old African Grey parrot called Chanel. Create an object for her.
+ */
+
+ parrot = {
+    name: "Chanel",
+    age: 12,
+    breed: "African Grey",
+    colours: ["grey", "white"]
+ };
+
+ console.log(parrot);
